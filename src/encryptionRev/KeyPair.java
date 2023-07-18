a4fb16ec6dff7dde94f40ead9a0c722b4208e472
Maksim Efimchick <maksefimchic@gmail.com>
2023-07-18 11:48:38 +0300

    add revision

---
+++ src/encryptionRev/KeyPair.java
@@ -0,0 +1,12 @@
+package encryptionRev;
+
+public class KeyPair {
+
+    public final byte[] k1;
+    public final byte[] k2;
+
+    public KeyPair(byte[] k1, byte[] k2) {
+        this.k1 = k1;
+        this.k2 = k2;
+    }
+}
